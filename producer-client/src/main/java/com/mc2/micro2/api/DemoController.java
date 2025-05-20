a8fd2dd150c5d363f09e2f2be6770504f6569bd2
Fiso Nleya <fiso.nleya@swiss-authentis.ch>
2025-05-20 18:48:52 +0200

    first commit

---
+++ producer-client/src/main/java/com/mc2/micro2/api/DemoController.java
@@ -0,0 +1,23 @@
+//package com.mc2.micro2.api;
+//
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/serviceB")
+//public class DemoController {
+//
+//
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/greeting")
+//    String getGreeting() {
+//        return "Hello from microservice 2";
+//    }
+//
+//
+//
+//}
